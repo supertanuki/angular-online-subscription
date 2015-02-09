72c0ef1b2dcf5de41a733d1d39302d1b5adc4dfc
supertanuki <supertanuki@gmail.com>
2015-02-09 17:23:08 +0100

    select all block when a session is selected

--- public/components/view/toggle-radio.js
+++ public/components/view/toggle-radio.js
@@ -15,11 +15,17 @@ $(function(){
 			$radio = $(this).find('input[name='+$name+']')
 			if ($radio.prop('checked') && $input.prop('id') != $radio.prop('id')) {
 				$(this).removeClass('active')
+				$(this).parent().parent().parent().toggleClass('alert').toggleClass('alert-success');
 			}
 		})
 		
 		// check or uncheck this radio
 		$label.toggleClass('active')
 		$input.prop('checked', $label.hasClass('active')).trigger('change')
+		$parent = $label.parent().parent().parent();
+		
+		if ($parent.hasClass('thumbnail')) {
+			$parent.toggleClass('alert').toggleClass('alert-success');
+		}
 	})
 })
